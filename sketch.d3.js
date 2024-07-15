d26ab8a7140d8aaf8b45bb7d3d85045d3ebde34b
krokosik <wikkra@gmail.com>
2024-07-15 13:18:58 +0200

    Working brownian motion

--- sketch.d3.js
+++ sketch.d3.js
@@ -7,10 +7,16 @@ const links = [
   [3, 4],
 ].map((link) => ({ source: link[0], target: link[1] }));
 let gravityCenter; // Dynamic center based on window size
-const chargeStrength = 80;
-const linkDistance = 100;
-const nodeRadius = 30;
+const CHARGE_STRENGTH = 0.05;
+const LINK_DISTANCE = 10;
+const NODE_RADIUS = 30;
+const GAS_RADIUS = 3;
 const collisionRadius = 50;
+const GAS_DENSITY = 0.0005; // particles per sq px
+const NUM_DIFFUSERS = 5;
+const DIFFUSER_RADIUS = 50;
+let TEMP = 10;
+let numDustParticales;
 
 let orbs = [];
 let simulation;
@@ -22,38 +28,75 @@ function setup() {
 
   // Initialize orbs
   for (let i = 0; i < numOrbs; i++) {
-    orbs.push({ x: random(width), y: random(height) });
+    orbs.push({
+      x: random(width),
+      y: random(height),
+      vx: randomVelocity(TEMP),
+      vy: randomVelocity(TEMP),
+      r: NODE_RADIUS,
+    });
+  }
+
+  // Initialize gas
+  numDustParticales = GAS_DENSITY * width * height;
+  for (let i = 0; i < numDustParticales; i++) {
+    orbs.push({
+      x: random(width),
+      y: random(height),
+      vx: randomVelocity(TEMP),
+      vy: randomVelocity(TEMP),
+      r: GAS_RADIUS,
+    });
   }
 
   // Set up D3 simulation
   simulation = d3
     .forceSimulation(orbs)
-    .force("center", d3.forceCenter(gravityCenter.x, gravityCenter.y))
-    .force("charge", d3.forceManyBody().strength(chargeStrength))
-    .force("collision", d3.forceCollide().radius(collisionRadius))
+    // .force("center", d3.forceCenter(gravityCenter.x, gravityCenter.y))
+    .force("charge", d3.forceManyBody().strength(CHARGE_STRENGTH))
+    .force(
+      "bounce",
+      d3.forceBounce().radius((d) => d.r)
+    )
+    .force(
+      "surface",
+      d3
+        .forceSurface()
+        .surfaces([
+          { from: { x: 0, y: 0 }, to: { x: width, y: 0 } },
+          { from: { x: width, y: 0 }, to: { x: width, y: height } },
+          { from: { x: width, y: height }, to: { x: 0, y: height } },
+          { from: { x: 0, y: height }, to: { x: 0, y: 0 } },
+        ])
+        .radius((d) => d.r)
+    )
+    .alphaDecay(0)
+    .velocityDecay(0)
     .force(
       "link",
       d3
         .forceLink()
         .id((d, i) => i)
-        .distance(linkDistance)
+        .distance(LINK_DISTANCE)
     )
     .on("tick", ticked);
 
-  simulation.force("link").links(links);
+  // simulation.force("link").links(links);
 
   // Prevent simulation from freezing by continuously reheating
-  simulation.alphaTarget(0.3).restart();
+  // simulation.alphaTarget(0.3).restart();
 }
 
 function draw() {
-  background(0); // Background color similar to your image
+  background(0);
   fill(255);
   stroke(0, 0, 0, 0);
 
   // Draw orbs based on D3's force simulation calculations
   for (let orb of orbs) {
-    ellipse(orb.x, orb.y, 2 * nodeRadius);
+    if (orb.r != GAS_RADIUS) {
+      ellipse(orb.x, orb.y, 2 * orb.r);
+    }
   }
 }
 
@@ -87,8 +130,7 @@ function mouseReleased() {
   simulation.alpha(0.3).restart(); // Reheat the simulation
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  gravityCenter = { x: width / 2, y: height / 2 }; // Update gravity center
-  simulation.force("center", d3.forceCenter(gravityCenter.x, gravityCenter.y)); // Update D3 center force
+function randomVelocity(temp) {
+  // The Maxwell-Boltzman velocity distribution where temp is a renormalized temperature temp = kT/m
+  return d3.randomNormal(0, Math.sqrt(temp))();
 }
